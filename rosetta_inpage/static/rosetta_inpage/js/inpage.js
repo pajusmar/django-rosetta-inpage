b9212c69006c4f3e3a7698c046895ff122276b5f
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-02-20 15:02:25 +0100

    Javascript wrapped in a closure

--- rosetta_inpage/static/rosetta_inpage/js/inpage.js
+++ rosetta_inpage/static/rosetta_inpage/js/inpage.js
@@ -1,117 +1,173 @@
-/**
- * Functions for the translations
+/***
+ * Le JavaScript for Rosetta Inpage app.
+ * Depends on jQuery.
+ * https://github.com/citylive/django-rosetta-inpage
  *
- **/
-var RosettaInpage = {
-    'root': '/rosetta_inpage'
-};
-
-/**
+ * Everything is wrapped in a closure to encapsulate all functionality. Public functionality is exposed in the end
+ * of this file by assigning the Inpage object to the window object and expose it as RosettaInpage.
  *
+ *
+ * Ensure that jQuery and jQuery.form is loaded. If they're not loaded yet will do a document.write,
+ * this is a blocking statement and will assure that both libraries are loaded before we include this javascript file.
+ *
+ * <script>!window.jQuery && document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"><' + '/script>');</script>
+ * <script>!window.jQuery().ajaxForm && document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.24/jquery.form.js"><' + '/script>');</script>
  */
-RosettaInpage.init = function(){
+(function(window, document){
+    'use strict';
 
-};
-
-/**
- *
- * @param [e]
- */
-RosettaInpage.hideForm = function(e){
-    //$("#rosetta-inpage-form").hide();
-    //$("#rosetta-inpage-sidebar a").removeClass("active");
-};
-
-/**
- * This is a nifty trick to place the cursor at the end of the text when the onfocus event is triggered
- * onfocus="var val=this.value; this.value=''; this.value= val;"
- * http://stackoverflow.com/questions/511088/use-javascript-to-place-cursor-at-end-of-text-in-text-input-element
- *
- * @param obj
- */
-RosettaInpage.moveCursorToEnd = function(obj){
-    var value = obj.val(); //store the value of the element
-    $(obj).focus().val(value);
-};
-
-/**
- *
- * @returns {boolean}
- */
-RosettaInpage.submit = function(){
-    return false;
-};
+    // Define some constants
+    var ROOT = '/rosetta_inpage',
+        PREFIX = 'rosetta-inpage',
+        ID_SIDEBAR = PREFIX + '-sidebar',
+        ID_FORM = PREFIX + '-form'
+    ;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$("#rosetta-inpage-sidebar a").click(function(e){
-    var pos = $(this).position();
-    var width = $(this).outerWidth();
-    var form = $("#rosetta-inpage-form");
-
-    var input = form.find('input[name="source"]');
-    var textarea = form.find('textarea');
-
-    var source = $(this).parent().find('code[type="source"]');
-    var msg = $(this).parent().find('code[type="msg"]').html();
-    var translated = msg.substring(4, msg.length-3);
-
-    var next = $(this).parent().next();
-    form.find('input[name="next"]').val(next.children(':first').attr('id'));
-
-    form.show();
-    form.css({
-        'top': pos['top'],
-        'left': pos['left'] + width
-    });
-
-    input.val(source.html());
-    textarea.focus();
-    textarea.val(translated);
-    RosettaInpage.moveCursorToEnd(textarea);
-
-
-    $("#rosetta-inpage-sidebar a").removeClass("active");
-    $(this).addClass("active");
-    e.stopPropagation();
-
-    /*
-     source = request.POST.get('source', '')
-     target_locale = request.POST.get('lang', '')
-     target_msg = request.POST.get('msg', '')
-     print "Banaan = ", str(source), ", ", str(target_locale), ", ", str(target_msg)
+    /**
+     * Define the namespace. All functions in this namespace are public.
      */
-});
+    var Inpage = {};
 
-$("#rosetta-inpage-sidebar").scroll(RosettaInpage.hideForm);
 
-$("#rosetta-inpage-form").click(function(e){
-    e.stopPropagation();
-});
+    /**
+     * Initialize all links in the sidebar.  When the user clicks on it the form to translate will appear
+     */
+    function initLinks(){
+        $('#' + ID_SIDEBAR + ' a').click(function(e){
+            var pos = $(this).position();
+            var width = $(this).outerWidth();
+            var form = $('#' + ID_FORM);
 
-$(document).click(function(e){
-    //console.log($(e.target).is(".rosetta-inpage-form"));
-    RosettaInpage.hideForm();
-    //$("#rosetta-inpage-form").hide();
-});
+            var input = form.find('input[name="source"]');
+            var textarea = form.find('textarea');
 
+            var source = $(this).parent().find('code[type="source"]');
+            var msg = $(this).parent().find('code[type="msg"]').html();
+            var translated = msg.substring(4, msg.length-3);
+
+            var next = $(this).parent().next();
+            form.find('input[name="next"]').val(next.children(':first').attr('id'));
+
+            form.show();
+            form.css({
+                'top': pos['top'],
+                'left': pos['left'] + width
+            });
+
+            input.val(source.html());
+            textarea.focus();
+            textarea.val(translated);
+            moveCursorToEnd(textarea);
+
+
+            $('#' + ID_SIDEBAR + ' a').removeClass("active");
+            $(this).addClass("active");
+            e.stopPropagation();
+
+            /*
+             source = request.POST.get('source', '')
+             target_locale = request.POST.get('lang', '')
+             target_msg = request.POST.get('msg', '')
+             print "Banaan = ", str(source), ", ", str(target_locale), ", ", str(target_msg)
+             */
+        });
+    }
+
+
+    /**
+     * Attach ajax functionality to the translation form.
+     * Depends on http://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.24/jquery.form.js
+     */
+    function initForm(){
+        var form = $('#' + ID_FORM);
+        //form.find('textarea').attr('disabled', 'disabled');
+
+        form.find("form").ajaxForm({
+            beforeSubmit: function(data, jqForm, options){
+                console.log("1=" + data);
+                console.log("2=" + jqForm);
+                console.log("3=" + options);
+                $(jqForm).find('textarea').attr('disabled', 'disabled');
+                $(jqForm).find('input[type=submit]').attr('value', 'Saving ...');
+            },
+            success: function(responseText, statusText, xhr, jqForm){
+                console.log(JSON.stringify(responseText) + ", " + statusText);
+                $(jqForm).find('textarea').removeAttr('disabled');
+                $(jqForm).find('input[type="submit"]').attr('value', 'Save');
+
+                var nextId = $(jqForm).find('input[name="next"]').val();
+                $('#' + nextId).trigger('click');
+            }
+        });
+    }
+
+
+    /**
+     * Configure general events and controls
+     */
+    function initPage(){
+        $('#' + ID_SIDEBAR).scroll(hideForm);
+
+        $('#' + ID_FORM).click(function(e){
+            e.stopPropagation();
+        });
+
+        $(document).click(function(e){
+            //console.log($(e.target).is(".rosetta-inpage-form"));
+            hideForm();
+            //$('#' + ID_FORM).hide();
+        });
+    }
+
+
+    /**
+     *
+     * @param [e]
+     */
+    function hideForm(e){
+        $('#' + ID_FORM).hide();
+        $('#' + ID_SIDEBAR + ' a').removeClass("active");
+    }
+
+
+    /**
+     * This is a nifty trick to place the cursor at the end of the text when the onfocus event is triggered
+     * onfocus="var val=this.value; this.value=''; this.value= val;"
+     * http://stackoverflow.com/questions/511088/use-javascript-to-place-cursor-at-end-of-text-in-text-input-element
+     *
+     * @param obj
+     */
+    function moveCursorToEnd(obj){
+        var value = obj.val(); //store the value of the element
+        $(obj).focus().val(value);
+    }
+
+
+
+
+
+    /**
+     *
+     */
+    Inpage.init = function(){
+        initLinks();
+        initForm();
+        initPage();
+    };
+
+    /**
+     * Exposing functionality
+     * Remapping Inpage to RosettaInpage
+     */
+    if(!window.RosettaInpage){
+        window.RosettaInpage = Inpage;
+    }
+})(window, document);
 
 
 //Deprecated
+/*
 $("span[contenteditable]").click(function(e){
     var text = $(this).html();
     var pos = $(this).position();
@@ -122,47 +178,9 @@ $("span[contenteditable]").click(function(e){
     //$('#s-4a6a59aa75d28c79e8e9485d5019ddd9').trigger('click');
 });
 
-
-$("a").click(function(e)
-{
+$("a").click(function(e){
     var id = $(this).attr('id');
-
-    //console.log("DDD = " + id);
-    //e.preventDefault();
+    console.log("DDD = " + id);
+    e.preventDefault();
 });
-
-
-$(document).ready(function(){
-    /*
-    $.post('/rosetta_inpage/ajax/message', {
-        'source': 'Login',
-        'lang': 'nl',
-        'msg': 'inloggen'
-    }).done(function(data){
-        //alert(JSON.stringify(data));
-    });
-    */
-
-
-    var form = $("#rosetta-inpage-form");
-    //form.find('textarea').attr('disabled', 'disabled');
-
-    form.find("form").ajaxForm({
-        beforeSubmit: function(data, jqForm, options){
-            console.log("1=" + data);
-            console.log("2=" + jqForm);
-            console.log("3=" + options);
-            $(jqForm).find('textarea').attr('disabled', 'disabled');
-            $(jqForm).find('input[type=submit]').attr('value', 'Saving ...');
-        },
-        success: function(responseText, statusText, xhr, jqForm){
-            console.log(JSON.stringify(responseText) + ", " + statusText);
-            $(jqForm).find('textarea').removeAttr('disabled');
-            $(jqForm).find('input[type="submit"]').attr('value', 'Save');
-
-            var nextId = $(jqForm).find('input[name="next"]').val();
-            $('#' + nextId).trigger('click');
-        }
-    });
-
-});
+*/
